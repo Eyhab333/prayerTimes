9f3621a4251927ced4d641e89be38f5bb01fde7a
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-04-05 15:42:36 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,100 @@
+let cities = [
+  {
+    arName: "الرياض",
+    enName: "Ar Riyāḑ",
+  },
+  {
+    arName: "القصيم",
+    enName: "Al Qaşīm",
+  },
+  {
+    arName: "مكة المكرمة",
+    enName: "Makkah al Mukarramah",
+  },
+  {
+    arName: "تبوك",
+    enName: "Tabūk",
+  }
+];
+
+for (let city of cities) {
+  const content = `
+    <option>${city.arName}</option>
+  `;
+  document.getElementById("cities-select").innerHTML += content;
+}
+
+document
+  .getElementById("cities-select")
+  .addEventListener("change", function () {
+    document.getElementById("city-name").innerHTML = this.value;
+
+    let cityName = "";
+    for (let city of cities) {
+      if (city.arName == this.value) {
+        cityName = city.enName;
+      }
+    }
+    getPrayersTimingsOfCity(cityName);
+  });
+
+function getPrayersTimingsOfCity(cityName) {
+  let params = {
+    country: "SA",
+    city: cityName
+  };
+
+  axios
+    .get("http://api.aladhan.com/v1/timingsByCity", {
+      params: params,
+    })
+    .then(function (response) {
+      const timings = response.data.data.timings;
+      fillTimeForPrayer("Fajr", timings.Fajr);
+      fillTimeForPrayer("Sunrise", timings.Sunrise);
+      fillTimeForPrayer("Dhuhr", timings.Dhuhr);
+      fillTimeForPrayer("Asr", timings.Asr);
+      fillTimeForPrayer("Maghrib", timings.Maghrib);
+      fillTimeForPrayer("Isha", timings.Isha);
+
+      const weekday = response.data.data.date.hijri.weekday.ar;
+      const day = response.data.data.date.hijri.day;
+      const month = response.data.data.date.hijri.month.ar;
+      const year = response.data.data.date.hijri.year;
+      const date = `${weekday} ${day} ${month} ${year}`;
+
+      document.getElementById("date").innerHTML = date;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function fillTimeForPrayer(id, time) {
+  document.getElementById(id).innerHTML = time;
+}
+
+
+
+let params = {
+  country: "SA",
+  city: "Ar Riyāḑ"
+};
+
+axios
+  .get("http://api.aladhan.com/v1/timingsByCity", {
+    params: params,
+  })
+  .then(function (response) {
+    const timings = response.data.data.timings;
+    fillTimeForPrayer("Fajr", timings.Fajr);
+    fillTimeForPrayer("Sunrise", timings.Sunrise);
+    fillTimeForPrayer("Dhuhr", timings.Dhuhr);
+    fillTimeForPrayer("Asr", timings.Asr);
+    fillTimeForPrayer("Maghrib", timings.Maghrib);
+    fillTimeForPrayer("Isha", timings.Isha);
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
